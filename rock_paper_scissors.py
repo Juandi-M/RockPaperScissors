fcb0622d52d2bb92871e2bb97ee6f7818f6d01c1
Kimberly Villalobos <abhjyzz@mmm.com>
2021-05-20 22:52:46 -0600

    renamed variables and methods in a more human readable way

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,129 +2,130 @@ import random
 import os
 import time
 
+
 def clear():
-	os.system("clear")
+    os.system("clear")
 
 # Set of instructions for Rock-Paper-Scissors
-def rps_instructions():
 
-	print("***********")
-	print("Instructions for Rock-Paper-Scissors : ")
-	print("***********")
-	print("Rock crushes Scissors")
-	print("Scissors cuts Paper")
-	print("Paper covers Rock")
-	print("***********")
 
-def rps():
-	
-	global rps_table
-	global game_map
-	global name
+def instruction():
 
-	# Game Loop for each game of Rock-Paper-Scissors
-	while True:
+    print("***********")
+    print("Instructions for Rock-Paper-Scissors : ")
+    print("***********")
+    print("Rock crushes Scissors")
+    print("Scissors cuts Paper")
+    print("Paper covers Rock")
+    print("***********")
 
-		print("--------------------------------------")
-		print("\t\tMenu")
-		print("--------------------------------------")
-		print("Enter \"help\" for instructions")
-		print("Enter \"rock\",\"paper\",\"scissors\" to play")
-		print("Enter \"exit\" to quit")
-		print("--------------------------------------")
 
-		print("***********")
+def gameLogic():
 
-		# Player Input
-		inp = input("Enter your move : ")
+    global table
+    global game_map
+    global name
 
-		if inp == "help":
-			clear()
-			rps_instructions()
-			continue
-		elif inp == "exit":
-			clear()
-			break	
-		elif inp == "rock":
-			player_move = 0
-		elif inp == "paper":
-			player_move = 1		
-		elif inp == "scissors":
-			player_move = 2
-		else:
-			clear()
-			print("Wrong Input!!")
-			rps_instructions()	
-			continue
+    # Game Loop for each game of Rock-Paper-Scissors
+    while True:
 
-		print("Computer making a move....")
+        print("--------------------------------------")
+        print("\t\tMenu")
+        print("--------------------------------------")
+        print("Enter \"help\" for instructions")
+        print("Enter \"rock\",\"paper\",\"scissors\" to play")
+        print("Enter \"exit\" to quit")
+        print("--------------------------------------")
 
-		print("***********")
-		time.sleep(2)
+        print("***********")
 
-		# Get the computer move randomly
-		comp_move = random.randint(0, 2)
+        # Player Input
+        inp = input("Enter your move : ")
 
-		# Print the computer move
-		print("Computer chooses ", game_map[comp_move].upper())
+        if inp == "help":
+            clear()
+            instruction()
+            continue
+        elif inp == "exit":
+            clear()
+            break
+        elif inp == "rock":
+            player_move = 0
+        elif inp == "paper":
+            player_move = 1
+        elif inp == "scissors":
+            player_move = 2
+        else:
+            clear()
+            print("Wrong Input!!")
+            instruction()
+            continue
 
-		# Find the winner of the match
-		winner = rps_table[player_move][comp_move]
+        print("Computer making a move....")
 
-		# Declare the winner 
-		if winner == player_move:
-			print(name, "WINS!!!")
-		elif winner == comp_move:
-			print("COMPUTER WINS!!!")
-		else:
-			print("TIE GAME")
+        print("***********")
+        time.sleep(2)
 
-		print("***********")
-		time.sleep(2)
-		clear()
+        # Get the computer move randomly
+        comp_move = random.randint(0, 2)
+
+        # Print the computer move
+        print("Computer chooses ", game_map[comp_move].upper())
+
+        # Find the winner of the match
+        winner = table[player_move][comp_move]
+
+        # Declare the winner
+        if winner == player_move:
+            print(name, "WINS!!!")
+        elif winner == comp_move:
+            print("COMPUTER WINS!!!")
+        else:
+            print("TIE GAME")
+
+        print("***********")
+        time.sleep(2)
+        clear()
 
 
 # The main function
 if __name__ == '__main__':
 
-	# The mapping between moves and numbers
-	game_map = {0:"rock", 1:"paper", 2:"scissors"}
+    # The mapping between moves and numbers
+    game_map = {0: "rock", 1: "paper", 2: "scissors"}
 
-	# Win-lose matrix for traditional game
-	rps_table = [[-1, 1, 0], [1, -1, 2], [0, 2, -1]]
-	
-	name = input("Enter your name: ")
+    # Win-lose matrix for traditional game
+    table = [[-1, 1, 0], [1, -1, 2], [0, 2, -1]]
 
-	# The GAME LOOP
-	while True:
+    name = input("Enter your name: ")
 
-		# The Game Menu
-		print("***********")
-		print("Let's Play!!!")
-		print("Enter 1 to play Rock-Paper-Scissors")
-		print("Enter 2 to quit")
-		print("***********")
+    # The GAME LOOP
+    while True:
 
-		# Try block to handle the player choice 
-		try:
-			choice = int(input("Enter your choice = "))
-		except ValueError:
-			clear()
-			print("Wrong Choice")	
-			continue
+        # The Game Menu
+        print("***********")
+        print("Let's Play!!!")
+        print("Enter 1 to play Rock-Paper-Scissors")
+        print("Enter 2 to quit")
+        print("***********")
 
-		# Play the traditional version of the game
-		if choice == 1:
-			rps()
+        # Try block to handle the player choice
+        try:
+            choice = int(input("Enter your choice = "))
+        except ValueError:
+            clear()
+            print("Wrong Choice")
+            continue
 
-		# Quit the GAME LOOP 	
-		elif choice == 2:
-			break
+        # Play the traditional version of the game
+        if choice == 1:
+            gameLogic()
 
-		# Other wrong input
-		else:
-			clear()
-			print("Wrong choice. Read instructions carefully.")
+        # Quit the GAME LOOP
+        elif choice == 2:
+            break
 
-							
-
+        # Other wrong input
+        else:
+            clear()
+            print("Wrong choice. Read instructions carefully.")
